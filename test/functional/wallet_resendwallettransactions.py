0608a160c7de0ea6f42a3e78c46393b86d105c34
Jon Atack <jon@atack.com>
2019-03-23 14:48:44 +0100

    [tests] Update test for wallet rebroadcasts
    Main change is the last `setmocktime` value
    from `setmocktime(0)` to `setmocktime(time_now + 60 * 29)`.

--- test/functional/wallet_resendwallettransactions.py
+++ test/functional/wallet_resendwallettransactions.py
@@ -32,39 +32,43 @@ class ResendWalletTransactionsTest(BitcoinTestFramework):
         self.skip_if_no_wallet()
 
     def run_test(self):
-        self.nodes[0].add_p2p_connection(P2PStoreTxInvs())
+        node = self.nodes[0]
+        node.add_p2p_connection(P2PStoreTxInvs())
 
         # Create a new transaction. Set time to 31 minutes in the past.
-        time_now = int(time.time())
-        self.nodes[0].setmocktime(time_now - 60 * 31)
-        txid = self.nodes[0].sendtoaddress(self.nodes[0].getnewaddress(), 1)
+        node.setmocktime(int(time.time()) - 60 * 31)
+        txid = node.sendtoaddress(node.getnewaddress(), 1)
 
-        # Can take a few seconds due to transaction trickling
-        wait_until(lambda: self.nodes[0].p2p.tx_invs_received[txid] >= 1)
+        # Can take a few seconds due to transaction trickling.
+        wait_until(lambda: node.p2p.tx_invs_received[txid] >= 1)
         time_now = int(time.time())
 
-        # Add a second peer since txs aren't rebroadcast to the same peer (see filterInventoryKnown)
-        self.nodes[0].add_p2p_connection(P2PStoreTxInvs())
+        # Add a second peer since txs aren't rebroadcast to the same peer (see filterInventoryKnown).
+        node.add_p2p_connection(P2PStoreTxInvs())
 
         # Mine a block. Transactions are only rebroadcast if a block has been mined
         # since the last time we tried to broadcast. Make sure that the transaction is
         # not included in the block.
         # The block must be received more than 5 minutes after the transaction timestamp for the
         # transaction to be rebroadcast.
-        self.nodes[0].setmocktime(time_now - 60 * 25)
-        block = create_block(int(self.nodes[0].getbestblockhash(), 16), create_coinbase(self.nodes[0].getblockchaininfo()['blocks']), time_now)
+        node.setmocktime(time_now - 60 * 25)
+        block = create_block(
+            int(node.getbestblockhash(), 16),
+            create_coinbase(node.getblockchaininfo()['blocks']),
+            time_now,
+        )
         block.nVersion = 3
         block.rehash()
         block.solve()
-        self.nodes[0].submitblock(ToHex(block))
+        node.submitblock(ToHex(block))
 
         # Transaction should not be rebroadcast
-        self.nodes[0].p2ps[1].sync_with_ping()
-        assert_equal(self.nodes[0].p2ps[1].tx_invs_received[txid], 0)
+        node.p2ps[1].sync_with_ping()
+        assert_equal(node.p2ps[1].tx_invs_received[txid], 0)
 
-        # Transaction should be broadcast after 30 minutes. Give an extra minute to be sure
-        self.nodes[0].setmocktime(0)
-        wait_until(lambda: self.nodes[0].p2ps[1].tx_invs_received[txid] >= 1)
+        # Transaction should be broadcast after 30 minutes. Give it ~60 (31 + 29) to be sure.
+        node.setmocktime(time_now + 60 * 29)
+        wait_until(lambda: node.p2ps[1].tx_invs_received[txid] >= 1)
 
 if __name__ == '__main__':
     ResendWalletTransactionsTest().main()
