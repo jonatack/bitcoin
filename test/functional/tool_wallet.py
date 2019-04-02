886d6b5a4e7ed23ef0cf48d2dbfbf82443af3c83
Jon Atack <jon@atack.com>
2019-04-02 16:38:47 +0200

    Second pass

--- test/functional/tool_wallet.py
+++ test/functional/tool_wallet.py
@@ -10,7 +10,7 @@ import subprocess
 import textwrap
 
 from test_framework.test_framework import BitcoinTestFramework
-from test_framework.util import assert_equal, assert_greater_than
+from test_framework.util import assert_equal
 
 class ToolWalletTest(BitcoinTestFramework):
     def set_test_params(self):
@@ -21,7 +21,7 @@ class ToolWalletTest(BitcoinTestFramework):
         self.skip_if_no_wallet()
 
     def bitcoin_wallet_process(self, *args):
-        binary = self.config["environment"]["BUILDDIR"] + '/src/bitcoin-wallet' + self.config["environment"]["EXEEXT"]
+        binary = self.config['environment']['BUILDDIR'] + '/src/bitcoin-wallet' + self.config['environment']['EXEEXT']
         args = ['-datadir={}'.format(self.nodes[0].datadir), '-regtest'] + list(args)
         return subprocess.Popen([binary] + args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
@@ -40,7 +40,7 @@ class ToolWalletTest(BitcoinTestFramework):
         assert_equal(stdout, output)
 
     def wallet_path(self):
-        return os.path.join(self.nodes[0].datadir, "regtest", "wallets", "wallet.dat")
+        return os.path.join(self.nodes[0].datadir, 'regtest', 'wallets', 'wallet.dat')
 
     def wallet_shasum(self):
         h = hashlib.sha1()
@@ -56,9 +56,9 @@ class ToolWalletTest(BitcoinTestFramework):
         self.log.info('Wallet file timestamp {0}: {1}'.format(op, timestamp))
         return timestamp
 
-    def log_wallet_timestamp_comparison(self, new, old):
-        result = 'unchanged' if new == old else 'increased! **FIX ME**'
-        self.log.info('Wallet file timestamp {0}\n'.format(result))
+    def log_wallet_timestamp_comparison(self, old, new):
+        result = 'unchanged' if new == old else 'increased! FIX ME'
+        self.log.info('Wallet file timestamp {}\n'.format(result))
 
     def run_test(self):
         self.log.info('Test that various invalid commands raise with specific error messages')
@@ -74,17 +74,15 @@ class ToolWalletTest(BitcoinTestFramework):
         self.log.info('Stop node\n')
         self.stop_node(0)
 
-        # Wallet tool info should work with wallet file permissions set to read-only.
-        # This needs to be fixed.
-        self.log.info('Set wallet file permissions to read-only')
-        os.chmod(self.wallet_path(), 0o400)
         self.log.info('Call wallet tool info')
-        self.assert_raises_tool_error('Error loading . Is wallet being used by another process?', 'info')
-        self.log.info('Wallet tool info command raises loading error! **FIX ME**')
-        self.log.info('Set wallet file permissions back to read/write\n')
-        os.chmod(self.wallet_path(), 0o600)
-
-        self.log.info('Call wallet tool info')
+        #
+        # TODO: Wallet tool info should work with wallet file permissions set to read-only,
+        # e.g. without raising 'Error loading . Is wallet being used by another process?'
+        # when the following 2 lines are uncommented:
+        #
+        # self.log.info('Set wallet file permissions to read-only')
+        # os.chmod(self.wallet_path(), 0o400)
+        #
         shasum_before = self.wallet_shasum()
         timestamp_before = self.wallet_timestamp('before info')
         out = textwrap.dedent('''\
@@ -99,14 +97,17 @@ class ToolWalletTest(BitcoinTestFramework):
         self.assert_tool_output(out, '-wallet=wallet.dat', 'info')
         shasum_after = self.wallet_shasum()
         timestamp_after = self.wallet_timestamp(' after info')
-        assert_equal(shasum_after, shasum_before)
+        self.log.info('Set wallet file permissions to read/write')
+        os.chmod(self.wallet_path(), 0o600)
+        assert_equal(shasum_before, shasum_after)
         self.log.info('Wallet file shasum unchanged')
-        self.log_wallet_timestamp_comparison(timestamp_after, timestamp_before)
-        # Wallet tool info should not write to the wallet file.
-        # This needs to be fixed.
-        assert_greater_than(timestamp_after, timestamp_before)
+        self.log_wallet_timestamp_comparison(timestamp_before, timestamp_after)
+        #
+        # TODO: Wallet tool info should not write to the wallet file.
+        # This assertion should be uncommented and succeed:
+        # assert_equal(timestamp_before, timestamp_after)
 
-        # Mutate the wallet to check the info command output changes accordingly.
+        # Mutate the wallet to verify that the info command output changes accordingly.
         self.log.info('Start node')
         self.start_node(0)
 
@@ -131,12 +132,13 @@ class ToolWalletTest(BitcoinTestFramework):
         self.assert_tool_output(out, '-wallet=wallet.dat', 'info')
         shasum_after = self.wallet_shasum()
         timestamp_after = self.wallet_timestamp(' after info')
-        assert_equal(shasum_after, shasum_before)
+        assert_equal(shasum_before, shasum_after)
         self.log.info('Wallet file shasum unchanged')
-        self.log_wallet_timestamp_comparison(timestamp_after, timestamp_before)
-        # Wallet tool info should not write to the wallet file.
-        # This needs to be fixed.
-        assert_greater_than(timestamp_after, timestamp_before)
+        self.log_wallet_timestamp_comparison(timestamp_before, timestamp_after)
+        #
+        # TODO: Wallet tool info should not write to the wallet file.
+        # This assertion should be uncommented and succeed:
+        # assert_equal(timestamp_before, timestamp_after)
 
         self.log.info('Call wallet tool create')
         shasum_before = self.wallet_shasum()
@@ -154,10 +156,10 @@ class ToolWalletTest(BitcoinTestFramework):
         self.assert_tool_output(out, '-wallet=foo', 'create')
         shasum_after = self.wallet_shasum()
         timestamp_after = self.wallet_timestamp(' after create')
-        assert_equal(shasum_after, shasum_before)
+        assert_equal(shasum_before, shasum_after)
         self.log.info('Wallet file shasum unchanged')
-        self.log_wallet_timestamp_comparison(timestamp_after, timestamp_before)
-        assert_equal(timestamp_after, timestamp_before)
+        self.log_wallet_timestamp_comparison(timestamp_before, timestamp_after)
+        assert_equal(timestamp_before, timestamp_after)
 
         self.log.info('Start node with arg -wallet=foo')
         self.start_node(0, ['-wallet=foo'])
@@ -174,8 +176,8 @@ class ToolWalletTest(BitcoinTestFramework):
         assert_equal(True, 'hdseedid' in out)
         assert_equal(shasum_after, shasum_before)
         self.log.info('Wallet file shasum unchanged')
-        self.log_wallet_timestamp_comparison(timestamp_after, timestamp_before)
-        assert_equal(timestamp_after, timestamp_before)
+        self.log_wallet_timestamp_comparison(timestamp_before, timestamp_after)
+        assert_equal(timestamp_before, timestamp_after)
 
         self.log.info('Stop node')
         self.stop_node(0)
