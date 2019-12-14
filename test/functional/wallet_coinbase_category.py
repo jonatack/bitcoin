28243d45a70f5462070e27a906ed813a1258eb70
Jon Atack <jon@atack.com>
2019-12-14 20:52:55 +0100

    test: add logging to wallet_coinbase_category.py

--- test/functional/wallet_coinbase_category.py
+++ test/functional/wallet_coinbase_category.py
@@ -32,28 +32,28 @@ class CoinbaseCategoryTest(BitcoinTestFramework):
 
     def run_test(self):
         node = self.nodes[0]
-        # Generate one block to an address
+        self.log.debug("Generate one block to an address")
         address = node.getnewaddress()
         node.generatetoaddress(1, address)
         hash = node.getbestblockhash()
         txid = node.getblock(hash)["tx"][0]
 
-        # Coinbase transaction is immature after 1 confirmation
+        self.log.info("Test coinbase txn is immature after 1 confirmation")
         assert_category(node, category="immature", address=address, txid=txid, skip=0)
 
-        # Mine another 99 blocks on top
+        self.log.debug("Mine another 99 blocks on top")
         node.generate(99)
-        # Coinbase transaction is still immature after 100 confirmations
+        self.log.info("Test coinbase txn is still immature after 100 confirmations")
         assert_category(node, category="immature", address=address, txid=txid, skip=99)
 
-        # Mine one more block
+        self.log.debug("Mine one more block")
         node.generate(1)
-        # Coinbase transaction is now matured, so category is "generate"
+        self.log.info("Test coinbase txn is mature and category is \"generate\" after 101 confirmations")
         assert_category(node, category="generate", address=address, txid=txid, skip=100)
 
-        # Orphan block that paid to address
+        self.log.debug("Orphan block that paid to address")
         node.invalidateblock(hash)
-        # Coinbase transaction is now orphaned
+        self.log.info("Test coinbase txn is orphaned after invalid block")
         assert_category(node, category="orphan", address=address, txid=txid, skip=100)
 
 if __name__ == '__main__':
