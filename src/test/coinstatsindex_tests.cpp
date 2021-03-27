f6ebc75c78442cf0fde8c4330ebdda32a426c1a3
Jon Atack <jon@atack.com>
2021-03-27 19:19:38 +0100

    PR19521 suggestions, commit dc26799 "Add unit test for Coinstats index"

--- src/test/coinstatsindex_tests.cpp
+++ src/test/coinstatsindex_tests.cpp
@@ -4,21 +4,24 @@
 
 #include <index/coinstatsindex.h>
 #include <test/util/setup_common.h>
+#include <util/time.h>
 #include <validation.h>
 
 #include <boost/test/unit_test.hpp>
+
+#include <chrono>
 
 BOOST_AUTO_TEST_SUITE(coinstatsindex_tests)
 
 BOOST_FIXTURE_TEST_CASE(coinstatsindex_initial_sync, TestChain100Setup)
 {
-    CoinStatsIndex coin_stats_index(1 << 20, true);
+    CoinStatsIndex coin_stats_index{1 << 20, true};
 
     CCoinsStats coin_stats;
     const CBlockIndex* block_index;
     {
         LOCK(cs_main);
-        block_index = ::ChainActive().Tip();
+        block_index = ChainActive().Tip();
     }
 
     // CoinStatsIndex should not be found before it is started.
@@ -32,27 +35,26 @@ BOOST_FIXTURE_TEST_CASE(coinstatsindex_initial_sync, TestChain100Setup)
 
     // Allow the CoinStatsIndex to catch up with the block index that is syncing
     // in a background thread.
-    constexpr int64_t timeout_ms = 120 * 1000;
-    int64_t time_start = GetTimeMillis();
+    const auto timeout = GetTime<std::chrono::seconds>() + 120s;
     while (!coin_stats_index.BlockUntilSyncedToCurrentChain()) {
-        BOOST_REQUIRE(time_start + timeout_ms > GetTimeMillis());
-        UninterruptibleSleep(std::chrono::milliseconds{100});
+        BOOST_REQUIRE(timeout > GetTime<std::chrono::milliseconds>());
+        UninterruptibleSleep(100ms);
     }
 
     // Check that CoinStatsIndex works for genesis block.
     const CBlockIndex* genesis_block_index;
     {
         LOCK(cs_main);
-        genesis_block_index = ::ChainActive().Genesis();
+        genesis_block_index = ChainActive().Genesis();
     }
     BOOST_CHECK(coin_stats_index.LookUpStats(genesis_block_index, coin_stats));
 
     // Check that CoinStatsIndex updates with new blocks.
     coin_stats_index.LookUpStats(block_index, coin_stats);
 
-    CScript scriptPubKey = CScript() << ToByteVector(coinbaseKey.GetPubKey()) << OP_CHECKSIG;
+    const CScript script_pub_key{CScript() << ToByteVector(coinbaseKey.GetPubKey()) << OP_CHECKSIG};
     std::vector<CMutableTransaction> noTxns;
-    CreateAndProcessBlock(noTxns, scriptPubKey);
+    CreateAndProcessBlock(noTxns, script_pub_key);
 
     // Let the CoinStatsIndex to catch up again.
     BOOST_CHECK(coin_stats_index.BlockUntilSyncedToCurrentChain());
@@ -61,7 +63,7 @@ BOOST_FIXTURE_TEST_CASE(coinstatsindex_initial_sync, TestChain100Setup)
     const CBlockIndex* new_block_index;
     {
         LOCK(cs_main);
-        new_block_index = ::ChainActive().Tip();
+        new_block_index = ChainActive().Tip();
     }
     coin_stats_index.LookUpStats(new_block_index, new_coin_stats);
 
